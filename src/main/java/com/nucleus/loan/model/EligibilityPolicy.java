de1339e9a2734daf94f4418a1e71b08df3069050
sushil92verma <sushil.verma@nucleussoftware.com>
2021-01-29 13:20:32 +0530

    model class

--- src/main/java/com/nucleus/loan/model/EligibilityPolicy.java
+++ src/main/java/com/nucleus/loan/model/EligibilityPolicy.java
@@ -1,13 +1,30 @@
 
 package com.nucleus.loan.model;
 
+import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
-
+@Entity(name = "EligibilityPolicy")
+@Table(name = "EligibilityPolicy")
 public class EligibilityPolicy {
+    @Id
+    @Column(name = "policyCode",length=10)
+    private String policyCode;
+    private String policyName;
+    private String policyDescription;
+    private Date createDate;
+    private String createdBy;
+    private Date modifiedDate;
+    private String modifiedBy;
+    private Date authorizedDate;
+    private String authorizedBy;
+    private PolicyStatus policyStatus;
 
-  
-
+    @OneToMany
+    private List<EligibilityParameter> eligibilityParameter;
+    @OneToMany
+    private List<Product> product;
 
 }
 
