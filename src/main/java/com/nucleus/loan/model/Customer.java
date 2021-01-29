8c08ffdd6b68b339b214475ed98f0d35fde12ebe
AthravAnkur <77144123+AthravAnkur@users.noreply.github.com>
2021-01-29 13:29:57 +0530

    commit by team5

---
+++ src/main/java/com/nucleus/loan/model/Customer.java
@@ -0,0 +1,141 @@
+package com.nucleus.loan.model;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.List;
+
+
+@Entity
+@Table(name = "Customer")
+public class Customer
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "customerCode",length = 20)
+    private int customerCode;
+    @Column(length = 30,nullable = false)
+    private String occupationType;
+    private String gender;
+    @Column(length = 30,nullable = false)
+    private String nationality;
+    @Column(length = 30,nullable = false)
+    private String firstName;
+    @Column(length = 30,nullable = false)
+    private String lastName;
+    @Column(length = 30,nullable = false)
+    private String fullName;
+    @Column(nullable = false)
+    private Date dateOfBirth;
+    @Column(length = 2)
+    private int workExperience;
+    @Column(length = 30)
+    private String organizationName;
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "customer")
+    private List<Address> address;
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "customer")
+    private List<LoanApplication> loanApplicationList;
+
+
+    public List<Address> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<Address> address) {
+        this.address = address;
+    }
+
+    public int getCustomerCode() {
+        return customerCode;
+    }
+
+    public void setCustomerCode(int customerCode) {
+        this.customerCode = customerCode;
+    }
+
+    public String getOccupationType() {
+        return occupationType;
+    }
+
+    public void setOccupationType(String occupationType) {
+        this.occupationType = occupationType;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getWorkExperience() {
+        return workExperience;
+    }
+
+    public void setWorkExperience(int workExperience) {
+        this.workExperience = workExperience;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "occupationType='" + occupationType + '\'' +
+                ", gender='" + gender + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", workExperience=" + workExperience +
+                ", organizationName='" + organizationName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/nucleus/loan/model/LoanApplication.java
@@ -0,0 +1,187 @@
+package com.nucleus.loan.model;
+
+import javax.persistence.*;
+
+@Entity
+public class LoanApplication
+{
+    @Id
+    @Column(length = 10,name = "LoanApplicationNumber")
+    private int loanApplicationNo;
+
+    @Column(name = "LoanAmountRequested",nullable = false)
+    private Double loanAmountRequested;
+    @Column(name = "Tenure",length = 3,nullable = false)
+    private int tenure;
+    @Column(name = "Rate",length = 2,nullable = false)
+    private Double rate;
+    @Column(name = "AgreementDate",nullable = false)
+    private String agreementDate;
+    @Column(name = "InstallmentDueDate",nullable = false)
+    private String installmentDueDate;
+    private String createdDate;
+    @Column(length = 30)
+    private String createdBy;
+    private String modifiedDate;
+    @Column(length = 30)
+    private String modifiedBy;
+    private String authorizedDate;
+    @Column(length = 30)
+    private String authorizedBy;
+    private String loanType;
+    private String disbursalStatus;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Customer customer;
+    private String product;
+
+    public int getLoanApplicationNo() {
+        return loanApplicationNo;
+    }
+
+    public void setLoanApplicationNo(int loanApplicationNo) {
+        this.loanApplicationNo = loanApplicationNo;
+    }
+
+    public Double getLoanAmountRequested() {
+        return loanAmountRequested;
+    }
+
+    public void setLoanAmountRequested(Double loanAmountRequested) {
+        this.loanAmountRequested = loanAmountRequested;
+    }
+
+    public int getTenure() {
+        return tenure;
+    }
+
+    public void setTenure(int tenure) {
+        this.tenure = tenure;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
+
+    public String getAgreementDate() {
+        return agreementDate;
+    }
+
+    public void setAgreementDate(String agreementDate) {
+        this.agreementDate = agreementDate;
+    }
+
+    public String getInstallmentDueDate() {
+        return installmentDueDate;
+    }
+
+    public void setInstallmentDueDate(String installmentDueDate) {
+        this.installmentDueDate = installmentDueDate;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public String getAuthorizedDate() {
+        return authorizedDate;
+    }
+
+    public void setAuthorizedDate(String authorizedDate) {
+        this.authorizedDate = authorizedDate;
+    }
+
+    public String getAuthorizedBy() {
+        return authorizedBy;
+    }
+
+    public void setAuthorizedBy(String authorizedBy) {
+        this.authorizedBy = authorizedBy;
+    }
+
+    public String getLoanType() {
+        return loanType;
+    }
+
+    public void setLoanType(String loanType) {
+        this.loanType = loanType;
+    }
+
+    public String getDisbursalStatus() {
+        return disbursalStatus;
+    }
+
+    public void setDisbursalStatus(String disbursalStatus) {
+        this.disbursalStatus = disbursalStatus;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    @Override
+    public String toString() {
+        return "LoanApplication{" +
+                "loanApplicationNo=" + loanApplicationNo +
+                ", loanAmountRequested=" + loanAmountRequested +
+                ", tenure=" + tenure +
+                ", rate=" + rate +
+                ", agreementDate='" + agreementDate + '\'' +
+                ", installmentDueDate='" + installmentDueDate + '\'' +
+                ", createdDate='" + createdDate + '\'' +
+                ", createdBy='" + createdBy + '\'' +
+                ", modifiedDate='" + modifiedDate + '\'' +
+                ", modifiedBy='" + modifiedBy + '\'' +
+                ", authorizedDate='" + authorizedDate + '\'' +
+                ", authorizedBy='" + authorizedBy + '\'' +
+                ", loanType='" + loanType + '\'' +
+                ", disbursalStatus='" + disbursalStatus + '\'' +
+                ", customer=" + customer +
+                ", product='" + product + '\'' +
+                '}';
+    }
+}
