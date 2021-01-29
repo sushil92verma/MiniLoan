71cca8a14ce409e0a878b3cc2b2e707ec2b4ecf9
sushil92verma <77203011+sushil92verma@users.noreply.github.com>
2021-01-29 14:47:04 +0530

    Update EligibilityPolicy.java

--- src/main/java/com/nucleus/loan/model/EligibilityPolicy.java
+++ src/main/java/com/nucleus/loan/model/EligibilityPolicy.java
@@ -1,18 +1,31 @@
-/*
+
 package com.nucleus.loan.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
-
+@Entity(name = "EligibilityPolicy")
+@Table(name = "EligibilityPolicy")
 public class EligibilityPolicy {
+    @Id
+    @Column(name = "policyCode",length=10)
+    private String policyCode;
+    private String policyName;
+    private String policyDescription;
+    private Date createDate;
+    private String createdBy;
+    private Date modifiedDate;
+    private String modifiedBy;
+    private Date authorizedDate;
+    private String authorizedBy;
+    private PolicyStatus policyStatus;
 
-  
-
+    @OneToMany
+    private List<EligibilityParameter> eligibilityParameter;
+    @OneToMany
+    private List<Product> product;
 
 }
 
-*/
+
