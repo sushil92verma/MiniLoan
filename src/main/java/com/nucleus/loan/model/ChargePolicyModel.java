3ab70e822aafd9904d6fae787206b81e5a863d06
ashish-10204 <Pankaj@123>
2021-01-29 12:45:56 +0530

    chargepolicycommit initial1

---
+++ src/main/java/com/nucleus/loan/model/ChargePolicyModel.java
@@ -0,0 +1,31 @@
+package com.nucleus.loan.model;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity(name = "ChargePolicy")
+@Table(name = "ChargePolicy")
+public class ChargePolicyModel {
+
+    @Id
+    private String chargePolicyCode;
+    private String chargePolicyName;
+    private String chargePolicyDescription;
+
+    private Date createDate;
+    private String createdBy;
+    private  Date modifiedDate;
+    private String modifiedBy;
+    private Date authorizedDate;
+    private String authorizedBy;
+    @OneToOne
+    private Charge charge;
+
+
+}
+
